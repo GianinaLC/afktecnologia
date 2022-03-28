60f087b55e351c4fa382d87d1f812429ddcffa90
glcarranzani <glcarranzani@gmail.com>
2022-03-28 20:23:53 -0300

    Se modifico el nombre de la carpeta

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,20 @@
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget';
+
+const NavBar = () => {
+    return(
+        <nav className="Menu">
+            <img src={'./images/afklogo1.png'} className="Logo-app" alt="logo" />
+            <ul>
+                <li>PC componentes</li>
+                <li>Conectividad</li>
+                <li>Accesorios</li>
+            </ul>
+            <CartWidget/>
+            
+        </nav>
+    );
+}
+
+export default NavBar;
+
