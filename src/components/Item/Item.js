9f1899ada9f0e33c5798498f5205645a63e0a0b0
glcarranzani <glcarranzani@gmail.com>
2022-04-01 11:45:34 -0300

    Se creó la lista de productos

---
+++ src/components/Item/Item.js
@@ -0,0 +1,15 @@
+import './Item.css'
+
+const Item = ({name, img, price, stock}) => {
+    return (
+        <section className="card">
+            <picture >
+                <img src={img} alt={name} className="cardImg"/>
+            </picture>
+            <h3 className="titleSize">{name}</h3>
+            <p className="price">$ {price} <span style={{color:'black', fontSize:'16px'}}>Stock: {stock}</span></p>
+            <button>Ver detalle</button>
+        </section>
+    )
+}
+export default Item
