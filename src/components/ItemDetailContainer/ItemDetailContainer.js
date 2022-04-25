462ab38a2d55dab2543000cd64d7737f1176f3e4
glcarranzani <glcarranzani@gmail.com>
2022-04-24 21:01:12 -0300

    Firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,22 +3,29 @@ import { getItem } from '../../asyncmock'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import './ItemDetailContainer.css'
 import {useParams} from 'react-router-dom'
-
+import { firestoreDb } from '../../services/firebase'
+import { getDoc, doc } from 'firebase/firestore'
 
 const ItemDetailContainer = () => {
     const [item, setItem] = useState()
-    const [loading, setLoading] = useState(true)
 
     //const params = useParams() devuelve un objeto, entonces para no tener que escribir despues params.productId (props), lo desestructuro
     const {productId} = useParams()
 
     useEffect (()=>{
-        getItem(productId).then(prod=>{
+        /* getItem(productId).then(prod=>{
             setItem(prod)
         }).catch(err  => {
             console.log(err)
         }).finally(() => {
             setLoading(false)
+        }) */
+
+        
+        getDoc(doc(firestoreDb, 'products', productId)).then(response => {
+            console.log(response)
+            const product = { id: response.id, ...response.data()}
+            setItem(product)
         })
 
         return (() => {
@@ -30,15 +37,12 @@ const ItemDetailContainer = () => {
 
     return ( 
         <div className='detailContainer'>
-            {   loading ?
-                <div className='spinnerContainer'><p className='spinner'></p></div>
-                :
+            {   
                 item ?
                     <ItemDetail {...item}/> 
                 :
                 <h2>El producto no existe</h2>
             }
-            {/*  dejo vacío el párrafo para que me tome el primer string de (cargando..) que se efectúa en el itemListContainer  */}
         </div>
     )
 }
---
+++ src/services/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAocG4rQZbD5zMpB55WOLoQs4nT9mJ_JY8",
+  authDomain: "ecommercetecno.firebaseapp.com",
+  projectId: "ecommercetecno",
+  storageBucket: "ecommercetecno.appspot.com",
+  messagingSenderId: "422631966906",
+  appId: "1:422631966906:web:89f7d10473d9c1a4099a83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firestoreDb = getFirestore(app)
